e040d3688a0689da0cbf5a2db006bc811e962753
RCFDark <2740487332@qq.com>
2026-04-08 10:30:17 +0800

    调试api缓存问题

--- src/api/config.js
+++ src/api/config.js
@@ -4,6 +4,8 @@ export const API_CONFIG = {
     'Content-Type': 'application/json',
     'Accept': '*/*',
     'ticket-token': 'a1c115f0-3d26-49e0-9b70-e05b85181eab',
-    'userId': 'ec359ef577b846be91a41d7ca17f9353'
+    'userId': 'ec359ef577b846be91a41d7ca17f9353',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache'
   }
 }
